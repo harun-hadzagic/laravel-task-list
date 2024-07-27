642535bad2ee6ac26ea0294a591a1dc101b04076
Harun Hadzagic <107354156+harun-hadzagic@users.noreply.github.com>
2024-07-28 00:36:34 +0200

    Adding links to individual tasks

--- task-list/resources/views/index.blade.php
+++ task-list/resources/views/index.blade.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>Hello {{$name}}, I am a blade template</h1>
+    <h1>Hello, I am a blade template</h1>
+    <div>
+        {{-- @if (count($tasks)) --}}
+        @forelse ($tasks as $task)
+            <a href="{{route('tasks.show', ['id'=>$task->id])}}">{{$task->title}}</a>
+            {{-- <div>{{ $task->title }}</div> --}}
+        @empty
+            <div>No tasks</div>
+        @endforelse
+        {{-- @endif --}}
+    </div>
 </body>
+
 </html>
