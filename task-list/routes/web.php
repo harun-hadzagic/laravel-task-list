b3f9370e88b99f95969bb19af7377ced1baf8142
Harun Hadzagic <107354156+harun-hadzagic@users.noreply.github.com>
2024-07-27 23:59:27 +0200

    Adding first blade

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -3,13 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return "Main page";
+    return view('index', [
+        'name' => 'Harun'
+    ]);
 });
 
 Route::get('/hello', function(){
     return "Hello";
+})->name('HELLO');
+
+Route::get('/hallo', function(){
+    return redirect()->route('HELLO');
 });
 
 Route::get('/greet/{name}', function($name){
     return 'Hello ' . $name . '!';
+});
+
+Route::fallback(function(){
+    return 'Still got somewhere';
 });
