ff022addf654fe51e1bdb8b711fc6720e5694637
Harun Hadzagic <107354156+harun-hadzagic@users.noreply.github.com>
2024-07-31 15:21:20 +0200

    App functionality completed

--- task-list/resources/views/index.blade.php
+++ task-list/resources/views/index.blade.php
@@ -2,12 +2,19 @@
 
 @section('title', 'The list of tasks')
 @section('content')
+
+    <div><a href="{{ route('task.create') }}">Add Task</a></div>
     {{-- @if (count($tasks)) --}}
     @forelse ($tasks as $task)
-        <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a><br/>
+        <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a><br />
         {{-- <div>{{ $task->title }}</div> --}}
     @empty
         <div>No tasks</div>
     @endforelse
     {{-- @endif --}}
+    @if ($tasks->count())
+        <nav>
+            {{ $tasks->links() }}
+        </nav>
+    @endif
 @endsection
--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -1,63 +1,58 @@
 <?php
 
+use App\Http\Requests\TaskRequest;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
-Route::get('/', function(){
+Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
-Route::get('/tasks', function (){
+Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => Task::latest()->get()
+        'tasks' => Task::latest()->paginate(10)
     ]);
 })->name('tasks.index');
 
 Route::view('/tasks/create', 'create')->name('task.create');
 
-Route::get('/tasks/{id}/edit', function($id) {
-    return view('edit', ['task'=> Task::findOrFail($id)]);
+Route::get('/tasks/{task}/edit', function (Task $task) {
+    return view('edit', ['task' => $task]);
 })->name('tasks.edit');
 
-Route::get('/tasks/{id}', function($id) {
-    return view('show', ['task'=> Task::findOrFail($id)]);
+Route::get('/tasks/{task}', function (Task $task) {
+    return view('show', ['task' => $task]);
 })->name('tasks.show');
 
 Route::fallback(function () {
     return 'Still got somewhere';
 });
 
-Route::post('/tasks', function(Request $request){
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required',
-    ]);
-    $task = new Task;
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();
+Route::post('/tasks', function (TaskRequest $request) {
 
-    return redirect()->route('tasks.show', ['id'=>$task->id])->with('success', 'Task created successfully!');
+    $task = Task::create($request->validated());
 
+    return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task created successfully!');
 })->name('tasks.store');
 
-Route::put('/tasks/{id}', function($id, Request $request){
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required',
-    ]);
-    $task = Task::findOrFail($id);
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
 
-    return redirect()->route('tasks.show', ['id'=>$task->id])->with('success', 'Task updated successfully!');
+    $task->update($request->validated());
 
+    return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task updated successfully!');
 })->name('tasks.update');
 
+
+Route::delete('tasks/{task}', function (Task $task) {
+    $task->delete();
+
+
+    return redirect()->route('tasks.index')->with('success', 'Task deleted successfully!');
+})->name('tasks.destroy');
+Route::put('tasks/{task}/toggle-complete', function (Task $task) {
+    $task->toggleComplete();
+    return redirect()->back()->with('success', 'Task updated successfully!');
+})->name('tasks.toggle-complete');
+ 
