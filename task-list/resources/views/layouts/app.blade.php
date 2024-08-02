66032d916b3291abf971d706f09eb3b01fd8c7a3
Harun Hadzagic <107354156+harun-hadzagic@users.noreply.github.com>
2024-08-02 21:26:19 +0200

    Adding tailwind

--- task-list/resources/views/layouts/app.blade.php
+++ task-list/resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Task List App</title>
+    <script src="https://cdn.tailwindcss.com"></script>
     @yield('styles')
 </head>
 <body>
